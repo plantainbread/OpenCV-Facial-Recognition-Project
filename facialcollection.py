596d7924ca63045cbd66cfe274ff5b114cee9102
plantainbread <119023568+plantainbread@users.noreply.github.com>
2024-08-03 11:02:17 -0700

    Update facialcollection.py

--- facialcollection.py
+++ facialcollection.py
@@ -1,14 +1,14 @@
 import cv2
 
-#turns on the camera
+# Turns on the webcam (you may have to change the number depending on which webcam you want to use, if you have more than one)
 video=cv2.VideoCapture(0)
 
-#face detection
+# Face detection
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-id = input("Enter your ID: ")
-# int(id) <-- Optional
+id = input("Enter an integer: ")
 count=0
+record_num = 500 # Change this if you wish to take more photos
 
 while True:
     ret,frame = video.read()
@@ -16,19 +16,20 @@ while True:
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         count=count+1
-        cv2.imwrite('datasets/User.'+str(id)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
+        # Photos taken will be placed in datasets directory
+        cv2.imwrite('datasets/User.' + id + '.'+str(count)+".jpg", gray[y:y+h, x:x+w])
         cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 2)
 
-    cv2.imshow("Frame",frame)
+    cv2.imshow("Window",frame)
 
     k=cv2.waitKey(1)
-
-    if count>500 or k == ord('q'):
+    # Terminates window once the count reaches set number or the 'q' key is pressed
+    if count>=record_num or k == ord('q'):
         break
 
-#stops video
+#Sstops video
 video.release()
 
-#closes video window
+# Closes video window
 cv2.destroyAllWindows()
-print("Dataset Collection Done..................")
+print("Collection process finished.")
